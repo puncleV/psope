7fe9c060050050852822a6309bad91595d16801a
Kozlov Victor <punkrus18@gmail.com>
2016-06-08 21:18:52 +0300

    Entry with diff users

--- index.php
+++ index.php
@@ -17,9 +17,10 @@
 		if (isset($_POST['login'], $_POST['password'])) {
 				$result = mysqli_query($idb, "SELECT * FROM `users` WHERE `login` = '".$_POST['login']."' AND `password` = '".$_POST['password']."'");
 			    if ($result->num_rows){
-			    	$_SESSION['id'] = $result->fetch_row()[0];
-			    	$_SESSION['fullName'] = $result->fetch_row()[1];
-			    	$_SESSION['group'] = $result->fetch_row()[0] == 1 ? "admin" : "user";
+			    	$row = $result->fetch_row();
+			    	$_SESSION['id'] = $row [0];
+			    	$_SESSION['fullName'] = $row [1];
+			    	$_SESSION['group'] = $row [2] == 1 ? "admin" : "user";
 			    }else{
 			     	errMsg("Пароль или адрес неверны");
 			    }
@@ -27,7 +28,7 @@
 			session_destroy();
 			header( 'Location: index.php', true, 301 );
 		}
-		
+
 		if(isset($_SESSION['group'])){
 			echo '<form target="index.php" method="POST">';
 			echo '<div class="form-group">';
--- user/user.php
+++ user/user.php
@@ -13,7 +13,6 @@
 				$uploadfile = $uploaddir .  $_SESSION['id'] . "_" . "1" . "_" . basename($_FILES['fileName']['name']);
 			}
 			$insertFile = mysqli_query($idb, "INSERT INTO `files` (file_type, size, path) VALUES ('".$_FILES["fileName"]["type"]."','".$_FILES["fileName"]["size"]."','".$uploadfile."')");
-			var_dump($uploadfile);
 			
 		 	if(move_uploaded_file($_FILES["fileName"]["tmp_name"], $uploadfile)){
 		 		gracMsg("Файл успешно загружен");
