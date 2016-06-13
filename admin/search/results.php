ae7b71158adecc5386ac41d3c4302bb916ffc3a1
Kozlov Victor <punkrus18@gmail.com>
2016-06-13 15:10:00 +0300

    установка квот

--- admin/search/results.php
+++ admin/search/results.php
@@ -2,6 +2,28 @@
 		<?php 
 			if(isset($_SESSION['gsearch']) || isset($_SESSION['usearch'])){
 				if(isset($_SESSION['usearch'])){
+					if(isset($_POST['quotaset'])){
+						$isql = mysqli_query($idb, "UPDATE `quotas` SET `all_quota` = '" . $_POST['quotaset'] . "' WHERE `user_id` = '" . $_SESSION['usearch'] . "'");
+					}elseif(isset($_POST['quotareset'])){
+						$isql = mysqli_query($idb, "UPDATE `quotas` SET `used_quota` = 0 WHERE `user_id` = '" . $_SESSION['usearch'] . "'");
+					}
+					$isql = mysqli_query($idb, "SELECT * FROM `users` WHERE `user_id` = " . $_SESSION['usearch']);
+					$userInfo = $isql->fetch_row();
+					$isql = mysqli_query($idb, "SELECT * FROM `quotas` WHERE `user_id` = " . $_SESSION['usearch']);
+					$quotaInfo = $isql->fetch_row();
+					echo '<form class="form-inline" method="POST">
+						  <div class="form-group">
+						    <label for="quotasSet"> Студент ' . $searchName . ' (Квота: ' . $quotaInfo[2] . '/' . $quotaInfo[1] . '; Всего: ' . $quotaInfo[3] . ')</label>
+						    <input type="text" class="form-control" id="quotasSet" name="quotaset" placeholder="Установить новую квоту">
+						  </div>
+						  <button type="submit" class="btn btn-default">Установить</button>
+						</form>
+						<form class="form" method="POST">
+						  <div class="form-group">
+						    <input type="hidden" class="form-control" id="quotasSet" name="quotareset" >
+						  </div>
+						  <button type="submit" class="btn btn-default">Сбросить использовнные</button>
+						</form>';
 					echo '<table class="table">
 						<thead>
 							<td>ФИО студента</td>
@@ -17,8 +39,6 @@
 						$page = 0;
 					}
 					$countRequest = mysqli_query($idb, "SELECT COUNT(*) FROM `files` WHERE `user_id` = " . $_SESSION['usearch']);
-					$isql = mysqli_query($idb, "SELECT * FROM `users` WHERE `user_id` = " . $_SESSION['usearch']);
-					$userInfo = $isql->fetch_row();
 					$isql = mysqli_query($idb, "SELECT `group_name` FROM `groups` WHERE `group_id` = " . $userInfo[2] );
 		 			$groupName = $isql->fetch_row()[0];
 					$isql = mysqli_query($idb, "SELECT * FROM `files` WHERE `user_id` = " . $_SESSION['usearch'] . " LIMIT " . $page * 10 . "," . 10);
@@ -37,12 +57,23 @@
 						echo "<p>Нет загруженных файлов</p>";
 					}
 				}else{
+					if(isset($_POST['quotaset'])){
+						$isql = mysqli_query($idb, "UPDATE `quotas` SET `all_quota` = '" . $_POST['quotaset'] . "' WHERE `user_id` IN (SELECT `user_id` FROM `users` WHERE `group_id` = " . $_SESSION['gsearch'] . ")");
+					}elseif(isset($_POST['quotareset'])){
+						$isql = mysqli_query($idb, "UPDATE `quotas` SET `used_quota` = 0 WHERE `user_id` IN (SELECT `user_id` FROM `users` WHERE `group_id` = " . $_SESSION['gsearch'] . ")");
+					}
 					echo '<form class="form-inline" method="POST">
 						  <div class="form-group">
 						    <label for="quotasSet"> Группа ' . $searchName . '</label>
 						    <input type="text" class="form-control" id="quotasSet" name="quotaset" placeholder="Установить квоту">
 						  </div>
 						  <button type="submit" class="btn btn-default">Установить</button>
+						</form>
+						<form class="form" method="POST">
+						  <div class="form-group">
+						    <input type="hidden" class="form-control" id="quotasSet" name="quotareset" >
+						  </div>
+						  <button type="submit" class="btn btn-default">Сбросить использовнные</button>
 						</form>';
 					echo '<table class="table">
 						<thead>
