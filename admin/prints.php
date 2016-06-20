d5de0bbfdd032a7bca971ea9b377e27bac8968e3
Kozlov Victor <punkrus18@gmail.com>
2016-06-20 08:17:33 +0300

    file

---
+++ admin/prints.php
@@ -0,0 +1,51 @@
+<div class="panel-body">
+	<div class="well">Ожидается печатей:  <?php echo $printRequests; ?></div>
+	<table class="table">
+		<thead>
+			<td>ФИО студента</td>
+			<td>Группа</td>
+			<td>Документ</td>
+			<td>Время печати</td>
+			<td>Количество страниц</td>
+		</thead>
+		<tbody>	
+		<?php
+			if(isset($_GET['pag'])){
+				$page = $_GET['pag'] - 1;
+			}else{
+				$page = 0;
+			}
+			$result = mysqli_query($idb, "SELECT * FROM `files` WHERE `status` = 0 LIMIT " . $page * $ROWSBYPAGE . "," . $ROWSBYPAGE);
+		 	if( $result->num_rows ){
+		 		$rows = $result->fetch_all();
+
+		 		foreach ($rows as $oRow) {
+		 			$isql = mysqli_query($idb, "SELECT * FROM `users` WHERE `user_id` = " . $oRow[1]);
+		 			$userInfo = $isql->fetch_row();
+		 			$isql = mysqli_query($idb, "SELECT `group_name` FROM `groups` WHERE `group_id` = " . $userInfo[2] );
+		 			$groupName = $isql->fetch_row()[0];
+					echo "<tr>";
+					echo "<td><a href='?page_1=2&usearch=".$userInfo[0]."'>" . $userInfo[1] . "</a></td>";
+					echo "<td><a href='?page_1=2&gsearch=".$userInfo[2]."'>" . $groupName . "</a></td>";
+					echo "<td>" . $oRow[2] . "</td>";
+					echo "<td>" . $oRow[9] . "</td>";
+					echo "<td>" . $oRow[5] . "</td>";
+					echo "</tr>";
+		 		}
+		 	}else{
+				echo "<p>Нет ожидаемых печатей</p>";
+			}
+		?>
+		</tbody>
+	</table>
+	<nav>
+		<?php
+			$countRequest = mysqli_query($idb, "SELECT COUNT(*) FROM `files` WHERE `status` = 0");
+			$rowsQuant = $countRequest->fetch_row()[0];
+			if( $rowsQuant > $ROWSBYPAGE){
+				$pagesCount = ceil($rowsQuant / $ROWSBYPAGE);
+				printPaginator($pagesCount, $currentPage);
+			}
+		?>
+	</nav>
+</div>
